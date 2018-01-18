2df48d6d631d0151e422010ade98a46f8c9dd167
Andriel Chaoti <accounts@vandesdelca32.net>
2018-01-17 19:01:20 -0700

    more project restructuring

--- interface/scripted/acwupgradetable/acwupgradetable.lua
+++ interface/scripted/acwupgradetable/acwupgradetable.lua
@@ -1,11 +1,15 @@
+
 function init()
-	if self then
-		sb.logInfo(sb.printJson(self))
-	end
-	sb.logInfo(_ENV)
+	local li = widget.addListItem("itemList")
+	widget.setItemSlotItem(widget.getChildAt(screenPosition), { name = "durasteelbar"})
+	widget.setData(li, {itemName = "Durasteel Bar"})
+	--widget.setListSelected("itemList", li)
+	--widget.setItemSlotItem(li .. ".itemIcon", {name="durasteelbar"});
+	--widget.setText(li .. ".itemName", "Durasteel Bar")
 end
 
 function update()
+
 end
 
 function uninit()
