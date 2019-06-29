d0d2cb1333d25972156d4c33659930f59ef9da0e
Andriel Chaoti <accounts@vandesdelca32.net>
2019-06-29 07:28:41 -0600

    Change to storage table to save between sessions.

--- objects/ct_wupgradetable/ct_wupgradeobject.lua
+++ objects/ct_wupgradetable/ct_wupgradeobject.lua
@@ -1,18 +1,18 @@
 function init(...)
-	if not self.upgradeState then
-		self.upgradeState = {
+	if storage and not storage.upgradeState then
+		storage.upgradeState = {
 			upgrading = false,
 			startTime = 0,
-			endTime = 0,
+			endTime = 0
 		}
 	end
 
 	message.setHandler("ct_UpgradeSet", function(...)
 		--sb.logInfo("%s", {...})
-		self.upgradeState, self.upgradeConfig = select(3,...)
+		storage.upgradeState, storage.upgradeConfig = select(3,...)
 	end)
 
 	message.setHandler("ct_UpgradeGet", function(...)
-		return {self.upgradeState, self.upgradeConfig or {}};
-	 end)
+		return {storage.upgradeState, storage.upgradeConfig or {}};
+	end)
 end
